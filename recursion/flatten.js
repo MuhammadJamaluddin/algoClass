d12ce26c9804ad37e0f63c1db6e9d6b440102b94
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2018-12-12 18:28:23 +0200

    Implement a function that flattens a nested array

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -4,3 +4,21 @@ Implement a function that flattens a nested array.
 flatten([1,[2],[3, [[4]]]]);
 => [1,2,3,4]
 */
+
+function flatten(inputArray) {
+  var flattenedElements = [];
+
+  function extract(array) {
+    array.forEach(function (element) {
+      if (!Array.isArray(element)) {
+        flattenedElements.push(element);
+      } else {
+        extract(element);
+      }
+    });
+  }
+
+  extract(inputArray);
+
+  return flattenedElements;
+}
